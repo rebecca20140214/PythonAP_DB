9a36aa350c0dc1249833a7b54922aef6a7fa4fa6
李雅琴 <41992551+rebecca20140214@users.noreply.github.com>
2024-03-02 11:55:24 +0800

    .

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -6,3 +6,38 @@ CREATE TABLE IF NOT EXISTS student(
 
 
 drop table student;
+
+
+CREATE TABLE IF NOT EXISTS artists(
+	id SERIAL PRIMARY KEY,
+	name varchar
+);
+
+DROP TABLE artists
+
+SELECT id, name  	FROM artists;
+
+
+SELECT max(length(name))  	FROM artists;  -- 85
+
+CREATE TABLE IF NOT EXISTS city(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30),
+	population INT
+);
+
+drop table city 
+
+CREATE TABLE IF NOT EXISTS city(
+	id SERIAL ,
+	name varchar(30),
+	population int,
+	PRIMARY KEY(id)
+);
+
+
+select * from city 
+
+
+
+
