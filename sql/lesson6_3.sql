d6b0f3319876d95d5517e14d1d51aca96c3d2fa2
李雅琴 <rebecca20140214@gmail.com>
2024-03-09 15:54:25 +0800

    .

--- sql/lesson6_3.sql
+++ sql/lesson6_3.sql
@@ -1,3 +1,22 @@
-select customer_id,count(*) as 筆數
-from payment
-group by customer_id ;
+/*取出payment的所有客戶的coutomer_id(不重覆)*/
+SELECT customer_id, COUNT(*) AS 筆數
+FROM payment
+GROUP BY customer_id
+
+/*取出payment的所有客戶的的應收帳款總合,SUM()*/
+SELECT customer_id, SUM(amount) AS 總合
+FROM payment
+GROUP BY customer_id
+ORDER BY 總合 DESC;
+
+SELECT first_name || ' ' || last_name AS full_name,
+	   SUM(amount) AS 總合
+FROM payment LEFT JOIN customer ON payment.customer_id=customer.customer_id
+GROUP BY full_name
+ORDER BY 總合 DESC;
+
+SELECT first_name || ' ' || last_name AS full_name,
+	   SUM(amount) AS 總合
+FROM payment LEFT JOIN customer USING (customer_id)
+GROUP BY full_name
+ORDER BY 總合 DESC;
